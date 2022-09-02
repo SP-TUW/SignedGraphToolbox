7024a55962404c58cf8938ec89cf6605de5aea6b
Thomas Dittrich <thomas.dittrich@tuwien.ac.at>
2022-09-02 15:19:44 +0200

    set number of simulation in sim_all as in DSLW 2022

--- src/launcher/SCLC/sim_all.py
+++ src/launcher/SCLC/sim_all.py
@@ -1,19 +1,22 @@
 from src.launcher.SCLC import init_sim, sbm_sim
 
+'''
+This script is capable of launching all simulations for our DSLW 2022 paper. Nevertheless, launching everything sequentially will take a huge amount of time. Instead we are using an HTCondor cluster to parallelize the simulations
+'''
 
 if __name__ == '__main__':
     init_sim.make_result_dirs()
-    for i in range(2):
+    for i in range(100):
         init_sim.run(i,0)
-    for i in range(2):
-        init_sim.run(i,1)
-    for i in range(2):
-        init_sim.run(i,2)
+    for i in range(100):
+        init_sim.run(i,3)
+    for i in range(100):
+        init_sim.run(i,6)
     init_sim.combine_results()
     init_sim.plot()
 
     sbm_sim.make_result_dirs()
-    for pid in range(2):
+    for pid in range(9900):
         sbm_sim.run(pid)
     sbm_sim.combine_results()
     sbm_sim.plot()
