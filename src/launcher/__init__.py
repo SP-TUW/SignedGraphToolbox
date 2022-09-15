a9c132a0e9359d237dd015f861f05744ded49565
Thomas Dittrich <thomas.dittrich@tuwien.ac.at>
2022-09-15 11:28:17 +0200

    optimize imports

--- src/launcher/__init__.py
+++ src/launcher/__init__.py
@@ -1,4 +1,4 @@
 # from src.launcher import *
 
-from src.launcher._sbm_simulation import SBMSimulation
 from src.launcher._classification_simulation import ClassificationSimulation
+from src.launcher._sbm_simulation import SBMSimulation
