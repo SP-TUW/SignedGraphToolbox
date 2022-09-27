ed92b7c525b331ab4f971c0a497d1cc91c60c5a7
Thomas Dittrich <thomas.dittrich@tuwien.ac.at>
2022-09-27 17:53:11 +0200

    include lsbm ml lelarge in sbm_sim and set simulations up

--- src/launcher/LSBM_MAP/sbm_sim.py
+++ src/launcher/LSBM_MAP/sbm_sim.py
@@ -5,7 +5,7 @@ import numpy as np
 
 from src.launcher import SBMSimulation
 from src.launcher.SCLC import constants
-from src.node_classification import LsbmMap
+from src.node_classification import LsbmMap, LsbmMlLelarge
 
 
 def make_result_dirs():
@@ -80,20 +80,46 @@ def plot():
     x_df.to_csv(os.path.join(constants.plots_dir['sbm_sim'], 'x.csv'))
 
 
-def get_graph_config_lists():
-    num_classes_list = [3, 5, 10]
-    num_nodes_list = [300 * nc for nc in num_classes_list]
-    class_distribution_list = [[1] * nc for nc in num_classes_list]
-    eps_list = np.linspace(0, 0.5, 11)
-    percentage_labeled_list = [0, 1, 5, 10, 15]
+def get_graph_config_lists(sim_id):
+    scale_pi = None
+    scale_pe = None
+    if sim_id == 0:
+        num_classes_list = [2, 2, 2]
+        num_nodes_list = [1000, 1000, 1000]
+        class_distribution_list = [[1, 1], [1, 1], [1, 1]]
+        percentage_labeled_list = [0, 0, 0]
+        scale_pi = [3/8, 6/8, 12/8]
+        scale_pe = [1/8, 4/8, 8/8]
+        eps_list = np.linspace(0, 0.5, 11)
+    else:
+        num_classes_list = [3, 5, 10]
+        num_nodes_list = [300 * nc for nc in num_classes_list]
+        class_distribution_list = [[1] * nc for nc in num_classes_list]
+        eps_list = np.linspace(0, 0.5, 11)
+        percentage_labeled_list = [0, 1, 5, 10, 15]
+
+    if scale_pi is None:
+        scale_pi = [1]*len(num_classes_list)
+    if scale_pe is None:
+        scale_pe = [1]*len(num_classes_list)
+
+    sbm_config_dict = {'num_classes': num_classes_list,
+                       'num_nodes': num_nodes_list,
+                       'class_distribution': class_distribution_list,
+                       'scale_pi': scale_pi,
+                       'scale_pe': scale_pe}
+    # for t in zip(*sbm_config_dict.values()) produces one slice over all lists
+    # dict(zip(sbm_config_dict.keys(),t)) combines each element in the slice with the corresponding key
+    # dict(zip(sbm_config_dict,t)) produces the same result -- we include .keys() for more clarity
+    sbm_config_list = [dict(zip(sbm_config_dict.keys(),t)) for t in zip(*sbm_config_dict.values())]
+
     config_lists = {'eps_list': eps_list,
                     'percentage_labeled_list': percentage_labeled_list,
-                    'sbm_config_list': [{'num_classes': cnd[0], 'num_nodes': cnd[1], 'class_distribution': cnd[2]}
-                                        for cnd in zip(num_classes_list, num_nodes_list, class_distribution_list)]}
+                    'sbm_config_list': sbm_config_list}
     return config_lists
 
 
-def get_methods(graph_config):
+def get_methods(graph_config, sim_id):
     pi = graph_config['pi']
     pe = graph_config['pe']
     li = graph_config['li']
@@ -102,27 +128,44 @@ def get_methods(graph_config):
     class_distribution = graph_config['class_distribution']
     eps = 1e-5
 
-    methods = [{'name': 'lsbm_map', 'method': LsbmMap(pi=pi, pe=pe, li=li, le=le, num_classes=num_classes,
-                                                      class_distribution=class_distribution, eps=eps, t_max=1e5,
-                                                      verbosity=1)},
-               ]
+    if sim_id == 0:
+        methods = [
+            {'name': 'lsbm_ml_lelarge', 'is_unsupervised': True,
+             'method': LsbmMlLelarge(pi=pi, pe=pe, li=li, le=le, num_classes=num_classes, verbosity=1)},
+            {'name': 'lsbm_map', 'is_unsupervised': True, 'l_guess': 'lsbm_ml_lelarge',
+             'method': LsbmMap(pi=pi, pe=pe, li=li, le=le, num_classes=num_classes,
+                               class_distribution=class_distribution, eps=eps, t_max=1e5, verbosity=1)},
+        ]
+    else:
+        methods = [
+            {'name': 'lsbm_map', 'method': LsbmMap(pi=pi, pe=pe, li=li, le=le, num_classes=num_classes,
+                                                   class_distribution=class_distribution, eps=eps, t_max=1e5,
+                                                   verbosity=1)},
+        ]
     return methods
 
 
-def run(pid):
-    config_lists = get_graph_config_lists()
+def run(pid, sim_id):
+    config_lists = get_graph_config_lists(sim_id)
 
     sim = SBMSimulation(**config_lists)
     graph_config, percentage_labeled, is_percentage = sim.get_graph_config(pid)
-    method_configs = get_methods(graph_config)
+    print('sbm with pi={pi} (a={a}) and pe={pe} (b={b})'.format(pi=graph_config['pi'], a=graph_config['num_nodes'] * graph_config['pi'], pe=graph_config['pe'], b=graph_config['num_nodes'] * graph_config['pe']))
+    method_configs = get_methods(graph_config, sim_id)
     sim.add_method(method_configs)
     sim.run_simulation(pid)
     sim.save_results(constants.results_dir['sbm_sim'], split_file=False)
 
+    if pid < len(config_lists):
+        filename = os.path.join(constants.plots_dir['sbm_sim'], 'x.json')
+        x_lists = {k: v.tolist() for k,v in sim.embedding.items()}
+        with open(filename, 'w') as x_file:
+            json.dump(x_lists, x_file)
+
 
 if __name__ == '__main__':
     import sys
-    from src.tools.simulation_tools import args_to_pid
+    from src.tools.simulation_tools import args_to_pid_and_sim_id
 
     args = sys.argv
 
@@ -141,5 +184,5 @@ if __name__ == '__main__':
             print('unknown command {c}'.format(c=args[1]))
         pass
     else:
-        pid = args_to_pid(sys.argv)
-        run(pid)
+        pid, sim_id = args_to_pid_and_sim_id(sys.argv)
+        run(pid, sim_id)
