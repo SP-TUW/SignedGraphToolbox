6947b8912b6ed712f9f7e1859b4b91ab1e5c5654
Thomas Dittrich <thomas.dittrich@tuwien.ac.at>
2022-09-30 18:21:46 +0200

    rename result folders

--- src/launcher/LSBM_MAP/constants.py
+++ src/launcher/LSBM_MAP/constants.py
@@ -3,8 +3,6 @@ import os
 results_dir = {'global': os.path.join('results', 'LSBM_MAP')}
 plots_dir = {'global': os.path.join(results_dir['global'], 'plots')}
 
-results_dir['sbm_sim'] = [os.path.join(results_dir['global'], 'sbm_sim_lelarge'),
-                          os.path.join(results_dir['global'], 'sbm_sim_hajek'),
-                          os.path.join(results_dir['global'], 'sbm_sim_yun')]
+results_dir['sbm_sim'] = [os.path.join(results_dir['global'], 'sbm_sim_{sid}'.format(sid=sim_id)) for sim_id in range(3)]
 
 plots_dir['sbm_sim'] = os.path.join(plots_dir['global'], 'sbm_sim')
