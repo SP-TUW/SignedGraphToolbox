e12580157a2300cffa8eee6caad1387186f40ffd
Thomas Dittrich <thomas.dittrich@tuwien.ac.at>
2022-11-15 12:33:36 +0100

    reduce number of configs

--- src/launcher/TV_CONVEX/sbm_sim.py
+++ src/launcher/TV_CONVEX/sbm_sim.py
@@ -238,8 +238,8 @@ def get_graph_config_lists(sim_id):
     elif sim_id == 6:
         num_classes_list = [3, 5, 10]
         percentage_labeled_list = [0, 10/3, 10, 20]
-        num_nodes_list = [900]
-        eps_list = np.linspace(0.0, 0.5*(1-1/100), 100)
+        num_nodes_list = [900]*3
+        eps_list = np.linspace(0.15, 0.35, 13)
     else:
         raise ValueError('unknown sim_id')
     class_distribution_list = [[1] * nc for nc in num_classes_list]
